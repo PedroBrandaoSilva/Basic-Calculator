d78d4c1aac106d7ade051ecdf37ad3925c797f0a
PedroBrandaoSilva <redpig825@gmail.com>
2025-04-23 10:56:17 -0300

    Resolve some bugs

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 let finalResult = document.querySelector("#screen")
+const buttonPlus = document.querySelector("#plus").innerHTML
+const buttonTimes = document.querySelector("#times").innerHTML
+const buttonMinus = document.querySelector("#minus").innerHTML
+const buttonDivide = document.querySelector("#divide").innerHTML
+const dot = document.querySelector("#num-dot").innerHTML
+
 let calculated = false
 
 
 
 function print(num){
 
+    if(num === buttonPlus || num === buttonDivide || num === buttonTimes || num === buttonMinus || num === dot){
+        calculated = false
+        
+
+    }
+
+
     if(calculated){
         document.querySelector("#screen").innerHTML = ""
         calculated = false
     }
-
+  
 
     if(finalResult.innerHTML.length <= 16 ){
         let number = document.querySelector("#screen").innerHTML
@@ -23,8 +36,9 @@ function cleanAll(){
 }
 
 function backSpace() {
-    let actualeResult = finalResult.innerHTML
-    document.querySelector("#screen").innerHTML = actualeResult.substring(0, actualeResult.length -1)
+        calculated = false
+        let actualeResult = finalResult.innerHTML
+        document.querySelector("#screen").innerHTML = actualeResult.substring(0, actualeResult.length -1)
 }
 
 function calc(){
